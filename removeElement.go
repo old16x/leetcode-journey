4ce2ffdf66f88a8cd527805df93dcb193fdbc043
rad16x <138368019+rad16x@users.noreply.github.com>
2023-07-10 14:36:20 +0200

    removeElement

--- removeElement.go
+++ removeElement.go
@@ -1,5 +1,13 @@
+// https://leetcode.com/problems/remove-element/
 package main
 
 func removeElement(nums []int, val int) int {
-
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+	return i
 }
--- removeElement_test.go
+++ removeElement_test.go
@@ -1,7 +1,22 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestRemoveElement(t *testing.T) {
+	nums := []int{3, 2, 2, 3}
+	val := 3
+	expectedResult := 2
+	expectedNums := []int{2, 2}
 
+	result := removeElement(nums, val)
+	if result != expectedResult {
+		t.Errorf("removeElement(nums, %d) = %d; want %d", val, result, expectedResult)
+	}
+
+	if !reflect.DeepEqual(nums[:result], expectedNums) {
+		t.Errorf("nums = %v; want %v", nums[:result], expectedNums)
+	}
 }
