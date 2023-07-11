070a3dfc5166d639e1235598f8990e872171218b
rad16x <138368019+rad16x@users.noreply.github.com>
2023-07-11 22:43:23 +0200

    removeDuplicatesFromSortedArray

---
+++ removeDuplicatesFromSortedArray.go
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+package main
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	k := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k
+}
